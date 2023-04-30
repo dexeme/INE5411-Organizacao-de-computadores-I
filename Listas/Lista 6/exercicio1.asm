503bc4915669eb9ef49e72d96efe5e00868b5634
dexeme <dexe.me12@gmail.com>
2023-04-30 15:13:14 -0300

    Update to use double type

--- Listas/Lista 6/exercicio1.asm
+++ Listas/Lista 6/exercicio1.asm
@@ -11,7 +11,7 @@
 # Área do círculo: pi*Raio^2
 
 .data
-	pi: .float 3.141592653589793
+	pi: .double 3.141592653589793
 	asksForRadius: .asciiz "Insira o raio do círculo: "
 	result: .asciiz "A área do círculo é: "
 .text
@@ -21,25 +21,25 @@ main:
 	li $v0, 4                   # Printa pergunta
 	syscall
 	
-	li $v0, 6 
+	li $v0, 7 
 	syscall			    # Lê valor do raio (float)
-	mov.s $f3, $f0              # Move pro $f3 o valor lido 
+	mov.d $f2, $f0              # Move pro $f3 o valor lido 
 	
-	mfc1 $t2, $f3		    # Coloca #f3 no registrador comum $t2
+	mfc1 $t2, $f2		    # Coloca #f3 no registrador comum $t2
 	blt $t2, 0, main            # Se raio < 0, pede novamente input
 	
 	la $t0, pi		    # Carrega endereço do pi
-	l.s $f1, ($t0)              # Lê float de precisão simples
+	l.d $f4, ($t0)              # Lê float de precisão simples
 	
-	mul.s $f2, $f0, $f0           # Raio² | Multiplicação entre precisão simples
-	mul.s $f2, $f2, $f1           # Raio² * pi
+	mul.d $f6, $f0, $f0           # Raio² | Multiplicação entre precisão simples
+	mul.d $f6, $f6, $f4           # Raio² * pi
 	
 	la $a0, result
 	li $v0, 4                   # Printa "A área do círculo é: "
 	syscall
 	
-	mov.s $f12, $f2             # $f12 é o registrador do float a ser impresso
-	li $v0, 2	            # Printa float
+	mov.d $f12, $f6             # $f12 é o registrador do float a ser impresso
+	li $v0, 3	            # Printa double
 	syscall
 	
 	
